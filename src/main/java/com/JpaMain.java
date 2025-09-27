ed299ee74a81d079b2d71aedf90b3723fea53ce2
9osari <128437395+9osari@users.noreply.github.com>
2025-09-27 18:36:49 +0900

    feat: JPA 기본편 학습 환경 구축 회원, 상품 Entity 생성

---
+++ src/main/java/com/JpaMain.java
@@ -0,0 +1,26 @@
+package com;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        //하나만 생성해 애플리케이션 전체 공유
+        //스레드간 공유 절대 X 쓰고 버린다.
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        tx.begin();
+        try {
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        }  finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
